da5bf603e8b928930af2f280c2de65230f9ee5d4
Léon Imanuel <leondino0309@gmail.com>
2025-02-18 14:59:03 +0100

    Added second camera for occlusion culling UI elements (lvl text) through Camera Stacking Seperated lvl text from player and update position in script instead to prevent rotation snapping weirdly

--- GameJam2025/Assets/Scripts/PointLvlTowardsCarmera.cs
+++ GameJam2025/Assets/Scripts/PointLvlTowardsCarmera.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
 
-public class PointTowardsCamera: MonoBehaviour
+public class PointTowardsCamera : MonoBehaviour
 {
     public Camera targetCamera;
+
+    private PlayerControler thePlayer;
+    [SerializeField] private Vector3 ofset;
+
+    private void Awake()
+    {
+        thePlayer = FindFirstObjectByType<PlayerControler>();
+        UpdatePositionRotation();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void LateUpdate()
     {
+        UpdatePositionRotation();
+    }
+
+    /// <summary>
+    /// Updates the position and rotation of the object to point towards the camera and follow the player
+    /// </summary>
+    private void UpdatePositionRotation()
+    {
+        transform.position = thePlayer.transform.position + ofset;
         transform.LookAt(targetCamera.transform);
         transform.rotation = Quaternion.LookRotation(targetCamera.transform.forward);
     }
