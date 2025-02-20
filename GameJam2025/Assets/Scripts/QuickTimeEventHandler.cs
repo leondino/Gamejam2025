246bdcfc70b13caf96f35e39585918d73200cb04
Léon Imanuel <leondino0309@gmail.com>
2025-02-20 13:45:01 +0100

    Created scene for quicktime events selected and editted UI images to use for a quicktime event button (prefab)

---
+++ GameJam2025/Assets/Scripts/QuickTimeEventHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class QuickTimeEventHandler : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartQuickTimeEvent()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartQuickTimeEvent();
+    }
+}
